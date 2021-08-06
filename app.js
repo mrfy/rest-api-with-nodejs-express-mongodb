3b6281d4e61ef1c6666f07bed60b3614ed3276d7
Grzegorz Fydrych <grzegorz.fydrych@synapstry.com>
2021-08-06 10:00:22 +0200

    update bcrypt

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const mongoose = require("mongoose");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.use(cors());
 app.use(bodyParser.json());
 
 //IMPORT ROUTE
-const postRoute = require('./routes/posts');
-const userRoute = require('./routes/user');
-const workRoute = require('./routes/work');
-const skillRoute = require('./routes/skills');
+const postRoute = require("./routes/posts");
+const userRoute = require("./routes/user");
+const workRoute = require("./routes/work");
+const skillRoute = require("./routes/skills");
 
 //INIT ROUTE
-app.use('/user',userRoute)
-app.use('/posts',postRoute)
-app.use('/work',workRoute)
-app.use('/skill',skillRoute)
+app.use("/user", userRoute);
+app.use("/posts", postRoute);
+app.use("/work", workRoute);
+app.use("/skill", skillRoute);
 
 //MONGODB CONNECTION
-mongoose.connect(process.env.DATABASE_URL,
-    { useNewUrlParser : true })
-    .then(() => console.log("MongoDB conected ..."))
-    .catch(err => console.log(err));
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB conected ..."))
+  .catch((err) => console.log(err));
 //START SERVER
-app.listen(8000)
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
