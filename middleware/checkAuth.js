1c03c66a6e686b71fb26cc9bf42055a434a8412b
Grzegorz Fydrych <grzegorz.fydrych@synapstry.com>
2021-08-06 15:20:02 +0200

    some fixes

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,12 +1,15 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-module.exports = (req,res,next)=>{
-    try {
-        const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_KEY)
-        res.userData = decoded;
-        next();
-    } catch (error) {
-        res.status(401).json({message : "Auth failed"})
-    }
-}
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    console.log("🚀 ~ file: checkAuth.js ~ line 6 ~ token", token);
+
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    console.log("🚀 ~ file: checkAuth.js ~ line 9 ~ decoded", decoded);
+    res.userData = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Auth failed" });
+  }
+};
