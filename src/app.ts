330cde082749f8cef84f6263c1e64a45d85cfd5b
Grzegorz Fydrych <grzegorzfydrych@gmail.com>
2021-08-12 22:43:01 +0200

    added morgan middleware

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,37 @@
-const express = require("express");
+import express from 'express';
+import morgan from 'morgan';
+//IMPORT ROUTEe
+import postRoute from './routes/posts';
+import skillRoute from './routes/skills';
+import userRoute from './routes/user';
+import workRoute from './routes/work';
+
 const app = express();
-const mongoose = require("mongoose");
-require("dotenv").config();
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const mongoose = require('mongoose');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(cors());
 app.use(bodyParser.json());
-
-//IMPORT ROUTEe
-import postRoute from "./routes/posts";
-import userRoute from "./routes/user";
-import workRoute from "./routes/work";
-import skillRoute from "./routes/skills";
+app.use(morgan('dev'));
 
 //INIT ROUTE
-app.use("/user", userRoute);
-app.use("/posts", postRoute);
-app.use("/work", workRoute);
-app.use("/skill", skillRoute);
+app.use('/user', userRoute);
+app.use('/posts', postRoute);
+app.use('/work', workRoute);
+app.use('/skill', skillRoute);
 
 //MONGODB CONNECTION
 mongoose
   .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
   })
-  .then(() => console.log("MongoDB conected ..."))
-  .catch((err: any) => console.log(err));
+  .then(() => console.log('MongoDB conected ...'))
+  .catch((err: any) => console.log('🚀 error !!', err));
 //START SERVER
 
 const port = process.env.PORT;
